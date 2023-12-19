95fb6386d9bcbdcd9916237e2a0bff66d37b83b9
mohammadtonor <54892954+mohammadtonor@users.noreply.github.com>
2023-12-19 23:47:05 +0330

    Update layout clerck

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider skipFetchCurrentUser={true}>
       <html lang="en">
         <body className={inter.className}>
           <ConfettiProvider />
